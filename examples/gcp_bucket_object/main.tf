e355c5f747004a34a8870a3247e30c57b90962fe
Aled Davies <aledsdavies@gmail.com>
2021-04-13 14:25:11 +0100

    Added an example and removed the default values

---
+++ examples/gcp_bucket_object/main.tf
@@ -0,0 +1,54 @@
+module "filter_files" {
+  source = "../.."
+
+  files_src    = "../build"
+  file_filters = [
+    {
+      regex : ".html$"
+      contentType  = "text/html"
+      cacheControl = "Cache-Control: max-age=0"
+    },
+    {
+      regex : ".css$"
+      contentType  = "text/css"
+      cacheControl = "Cache-Control: max-age=31536000, must-revalidate"
+    },
+    {
+      regex : ".js$"
+      contentType  = "text/javascript"
+      cacheControl = "Cache-Control: max-age=31536000, must-revalidate"
+    },
+    {
+      regex : ".json$"
+      contentType  = "text/html"
+      cacheControl = "Cache-Control: max-age=0"
+    },
+    {
+      regex : ".svg$"
+      contentType  = "image/svg+xml"
+      cacheControl = "Cache-Control: max-age=31536000, must-revalidate"
+    },
+    {
+      regex : "sitemap.xml$"
+      contentType  = "text/rss+xml"
+      cacheControl = "Cache-Control: max-age=0, s-max-age=600"
+    },
+  ]
+}
+
+variable "bucket" {
+  type    = string
+  default = "made_up"
+}
+
+
+resource "google_storage_bucket_object" "files" {
+  for_each = module.filter_files.files
+  name     = each.value.path
+  source = each.value.src
+  cache_control = each.value.cacheControl
+  content_type = each.value.contentType
+  bucket   = var.bucket
+
+
+}
--- variables.tf
+++ variables.tf
@@ -1,51 +1,13 @@
-variable "storage_bucket_name" {
-  type = string
-  default = "made_up"
-}
-
 /**
   The source location where you can find the files relative to the terraform
 */
 variable "files_src" {
-  type    = string
-  default = "./examples"
+  type = string
 }
 
 /**
-
+  The file filter use this to filter the files and add configurations
 */
 variable "file_filters" {
-  type    = list(map(any))
-  default = [
-    {
-      regex : ".html$"
-      contentType  = "text/html"
-      cacheControl = "Cache-Control: max-age=0"
-    },
-    {
-      regex : ".css$"
-      contentType  = "text/css"
-      cacheControl = "Cache-Control: max-age=31536000, must-revalidate"
-    },
-    {
-      regex : ".js$"
-      contentType  = "text/javascript"
-      cacheControl = "Cache-Control: max-age=31536000, must-revalidate"
-    },
-    {
-      regex : ".json$"
-      contentType  = "text/html"
-      cacheControl = "Cache-Control: max-age=0"
-    },
-    {
-      regex : ".svg$"
-      contentType  = "image/svg+xml"
-      cacheControl = "Cache-Control: max-age=31536000, must-revalidate"
-    },
-    {
-      regex : "sitemap.xml$"
-      contentType  = "text/rss+xml"
-      cacheControl = "Cache-Control: max-age=0, s-max-age=600"
-    },
-  ]
+  type = list(map(any))
 }
